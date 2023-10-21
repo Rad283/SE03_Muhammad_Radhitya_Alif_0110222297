1f0748db4167614b968f9f7a9f418e65e54b4c77
rad283 <ralif2135@gmail.com>
2023-10-21 17:21:37 +0700

    .

--- pertemuan_4/rest-api/app/Http/Controllers/AnimalController.php
+++ pertemuan_4/rest-api/app/Http/Controllers/AnimalController.php
@@ -7,21 +7,28 @@ use Illuminate\Http\Request;
 class AnimalController extends Controller
 {
 
-    public $animals = ['kucing', 'ikan', 'sapi'];
+    public $animals = ['kucing', 'ayam', 'ikan'];
 
     public function index()
     {
         # tampikan data animals
         echo "\n Menampilkan daftar nama hewan:";
         foreach ($this->animals as $animal) {
-            echo "\n" . $animal;
+            echo "\n- " . $animal;
         }
+        echo "\n\nNama: Muhammad Radhitya Alif\nNIM:0110222297\nKelas: SE03";
         // return json_encode($this->animals);
     }
 
     public function store(Request $request)
     {
         # menambahkan hewan baru
+
+        // mengecek apakah input kosong atau tidak
+        if (empty($request->nama)) {
+            return "input nama kosong";
+        }
+
         echo "Store - Menambahkan nama hewan: $request->nama";
         array_push($this->animals, $request->nama);
 
@@ -48,7 +55,7 @@ class AnimalController extends Controller
         if (!isset($this->animals[$id])) {
             echo "id tidak ada";
         } else {
-            array_splice($this->animals, $id);
+            unset($this->animals[$id]);
             echo "menghapus hewan dengan id $id";
 
             // memanggil method index untuk menampilkan daftar hewan
